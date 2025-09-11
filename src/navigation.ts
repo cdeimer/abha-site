4ac610af5dccc600fad8919d3710262e14f900eb
cdeimer <53084486+cdeimer@users.noreply.github.com>
2025-09-11 00:35:35 -0400

    Add Posts page (#22)
    * load documents from Notion, render list and individual pages
    
    * add styling to the document hub and individual document pages
    
    * working posts page
    
    * remove unnecessary comment
    
    * another npm audit
    
    * remove unused files
    
    * use repo secrets in actions
    
    * use secrets in build step too

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,6 +4,7 @@ export const headerData = {
   links: [
     //{ text: 'About Us', href: getPermalink('/coming-soon') },
     { text: 'Calendar', href: getPermalink('/calendar') },
+    // { text: 'Posts', href: getPermalink('/documents') }, // Uncomment when documents page is ready
     { text: 'Resources', href: getPermalink('/resources') },
     //{ text: 'Get Involved', href: getPermalink('/coming-soon') },
     { text: 'Contact', href: getPermalink('/contact') },
